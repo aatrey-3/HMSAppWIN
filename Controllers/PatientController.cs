75306a9305221d549eed40174e0f0bdc94645cc7
aatrey-3 <atreyadutta3@gmail.com>
2021-11-09 21:19:25 +0530

    default changes

--- Controllers/PatientController.cs
+++ Controllers/PatientController.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HMSAppWIN.Models;
 
 namespace HMSAppWIN.Controllers
 {
     public class PatientController : Controller
     {
-        // GET: Patient
         [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult List()
+        {
+            IList<HMSAppWIN.Models.Patient> patients = null;
+
+            try
+            {
+                using (var db = new HMSEntities())
+                {
+                    patients = db.Patients.ToList();
+                }
+            }
+            catch(Exception ex)
+            {
+                ViewData["ServerError"] = ex.Message;
+            }
+            return PartialView(patients);
         }
     }
 }
