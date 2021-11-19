4dc5e33d5f7ef511d6f6565ff5cc7a91a4344088
aatrey-3 <atreyadutta3@gmail.com>
2021-11-19 22:21:02 +0530

    changes: patient delete added

--- Controllers/PatientController.cs
+++ Controllers/PatientController.cs
@@ -23,7 +23,7 @@ namespace HMSAppWIN.Controllers
             {
                 using (var db = new HMSEntities())
                 {
-                    patients = db.Patients.ToList();
+                    patients = db.Patients.Include("Doctor").ToList();
                 }
             }
             catch(Exception ex)
@@ -33,35 +33,35 @@ namespace HMSAppWIN.Controllers
             return PartialView(patients);
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(Patient model)
-        {
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(Patient model)
+        //{
 
-            using (var db = new HMSEntities())
-            {
+        //    using (var db = new HMSEntities())
+        //    {
 
-                var patient = db.Patients.Where(x => x.ID == model.ID).FirstOrDefault();
+        //        var patient = db.Patients.Where(x => x.ID == model.ID).FirstOrDefault();
 
-                if (patient != null)
-                {
-                    patient.Name = model.Name;
-                    patient.Age = model.Age;
-                    patient.Gender = model.Gender;
-                    patient.Address = model.Address;
-                    patient.Disease = model.Disease;
-                    db.SaveChanges();
-                    ViewBag.Message = "Patient details updated successfully";
-                    return RedirectToAction("List");
-                }
-                else
-                {
-                    ViewBag.Message = "Invalid details! Please check the details carefully and try again";
-                    return View();
-                }
-            }
+        //        if (patient != null)
+        //        {
+        //            patient.Name = model.Name;
+        //            patient.Age = model.Age;
+        //            patient.Gender = model.Gender;
+        //            patient.Address = model.Address;
+        //            patient.Disease = model.Disease;
+        //            db.SaveChanges();
+        //            ViewBag.Message = "Patient details updated successfully";
+        //            return RedirectToAction("List");
+        //        }
+        //        else
+        //        {
+        //            ViewBag.Message = "Invalid details! Please check the details carefully and try again";
+        //            return View();
+        //        }
+        //    }
 
-        }
+        //}
 
         [HttpPost]
         public JsonResult Update(Patient model)
@@ -93,6 +93,42 @@ namespace HMSAppWIN.Controllers
                     else
                     {
                         res.Message = "Failed to update patient details";
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                res.Message = ex.Message;
+            }
+            return Json(res, JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpPost]
+        public JsonResult Delete(Patient model)
+        { //{"IsSuccess":"true","Message":""}
+            HMSResponse res = new HMSResponse();
+
+            try
+            {
+
+                using (var db = new HMSEntities())
+                {
+                    //JavaScriptSerializer deser = new JavaScriptSerializer();
+                    //Room model = deser.Deserialize<Room>(modeljson);
+                    var patient = db.Patients.Where(x => x.ID == model.ID).FirstOrDefault();
+
+                    if (patient != null)
+                    {
+                        db.Patients.Remove(patient);
+                        db.SaveChanges();
+                        res.IsSuccess = true;
+                        res.Message = "Patient record deleted successfully.";
+                    }
+                    else
+                    {
+                        res.Message = "Failed to find record to delete";
 
                     }
                 }
